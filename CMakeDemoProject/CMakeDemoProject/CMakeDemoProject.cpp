85933d0659d87cedb6b15bf228ea1d533bad3581
Adobe-Android <tngamerboy@gmail.com>
2019-04-21 14:57:07 -0500

    Added Visual Studio 2019 CMake project example

---
+++ CMakeDemoProject/CMakeDemoProject/CMakeDemoProject.cpp
@@ -0,0 +1,11 @@
+﻿// CMakeDemoProject.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeDemoProject.h"
+#include <fmt/core.h>
+
+int main()
+{
+	fmt::print("Hello {}!", "CMake");
+	return 0;
+}
