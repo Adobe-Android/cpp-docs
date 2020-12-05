e4b54c782b6ad56dc66d203d41edd53ce1cd6e44
David Brown <tngamerboy@gmail.com>
2020-12-04 21:58:37 -0800

    Fix formatting

--- src/compiler_version_check.cpp
+++ src/compiler_version_check.cpp
@@ -6,21 +6,21 @@ using std::string;
 using std::to_string;
 
 int main() {
-  // Uses C++11 features.
-  string compiler{};
+    // Uses C++11 features.
+    string compiler{};
 
-  #if defined(__clang__)
+#if defined(__clang__)
     compiler = "Clang version " __clang_version__ "\n";
-  #elif defined(__INTEL_COMPILER)
+#elif defined(__INTEL_COMPILER)
     compiler = "ICC version " + to_string(__ICC) + "\n";
-  #elif defined(__GNUC__)
+#elif defined(__GNUC__)
     compiler = "GCC version " __VERSION__ "\n";
-  #elif defined(_MSC_VER)
+#elif defined(_MSC_VER)
     compiler = "MSVC version " + to_string(_MSC_VER) + "\n";
-  #else
+#else
     compiler = "Unable to determine compiler";
-  #endif
-  
-  cout << compiler;
-  return 0;
+#endif
+
+    cout << compiler;
+    return 0;
 }
