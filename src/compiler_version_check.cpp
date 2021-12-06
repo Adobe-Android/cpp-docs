7305fd66d571ee3c73e85102b27728b5d0ee01c7
David Brown <tngamerboy@gmail.com>
2021-12-06 13:11:12 -0600

    Update compiler_version_check.cpp

--- src/compiler_version_check.cpp
+++ src/compiler_version_check.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-#include <string>
+#include <sstream>
 
-using std::cout;
-using std::string;
-using std::to_string;
+std::string get_compiler_info()
+{
+    std::stringstream compiler_str;
 
-int main() {
-    // Uses C++11 features.
-    string compiler{};
+    #if defined(__clang__)
+        compiler_str << "Clang version " << __clang_major__ << '.' << __clang_minor__ << '.' << __clang_patchlevel__;
+    #elif defined(__GNUC__) && !defined(__ICC)
+        compiler_str << "GCC version " << __VERSION__;
+    #elif defined(_MSC_VER)
+        compiler_str << "MSVC version " << _MSC_VER;
+    #elif defined(__INTEL_COMPILER)
+        compiler_str << "ICC version " << __ICC;
+    #else
+        compiler_str << "Unable to determine compiler"; 
+    #endif
 
-#if defined(__clang__)
-    compiler = "Clang version " __clang_version__ "\n";
-#elif defined(__INTEL_COMPILER)
-    compiler = "ICC version " + to_string(__ICC) + "\n";
-#elif defined(__GNUC__)
-    compiler = "GCC version " __VERSION__ "\n";
-#elif defined(_MSC_VER)
-    compiler = "MSVC version " + to_string(_MSC_VER) + "\n";
-#else
-    compiler = "Unable to determine compiler";
-#endif
+    return compiler_str.str();
+}
 
-    cout << compiler;
+int main()
+{
+    std::cout << get_compiler_info() << '\n';
     return 0;
 }
